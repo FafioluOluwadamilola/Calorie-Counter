737e245b0641b53603319ca895ce0d46db381a60
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2025-08-29 19:23:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+let isError = false;
+
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    str.replace(regex,"");
+    return str.replace(regex, "");
+}
+
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry(){
+  const targetId = entryDropdown.value;  
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll("input[type='string']").length;
+  const HTMLString  = `
+    <label for = "${entryDropdown.value}-${entryNumber}-name">
+        Entry ${entryNumber} Name
+    </label>  
+    <input type="text" placeholder="name" id="${entryDropdown.value}-${entryNumber}-name">
+
+    <label for = "${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" placeholder="calories" id="${entryDropdown.value}-${entryNumber}-calories">
+
+  `;
+}
